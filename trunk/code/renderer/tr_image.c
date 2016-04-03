7382fbe7539a986b20929b25d98ce6ab11fb6505
entdark <entdark@mail.ru>
2016-04-03 20:29:06 +0500

    added smooth dinamic light

--- trunk/code/renderer/tr_image.c
+++ trunk/code/renderer/tr_image.c
@@ -2031,30 +2031,40 @@ R_CreateDlightImage
 */
 #define	DLIGHT_SIZE	16
 static void R_CreateDlightImage( void ) {
-	int		x,y;
-	byte	data[DLIGHT_SIZE][DLIGHT_SIZE][4];
-	int		b;
+	int		width, height;
+	byte	*pic;
 
-	// make a centered inverse-square falloff blob for dynamic lighting
-	for (x=0 ; x<DLIGHT_SIZE ; x++) {
-		for (y=0 ; y<DLIGHT_SIZE ; y++) {
-			float	d;
+	R_LoadImage("data/dlight.tga", &pic, &width, &height);
+	if (pic) {                                    
+		Com_Printf("DLIGHT\n");
+		tr.dlightImage = R_CreateImage("*dlight", pic, width, height, qfalse, qfalse, GL_CLAMP );
+		Z_Free(pic);
+	} else { // if we dont get a successful load
+		int		x,y;
+		byte	data[DLIGHT_SIZE][DLIGHT_SIZE][4];
+		int		b;
 
-			d = ( DLIGHT_SIZE/2 - 0.5f - x ) * ( DLIGHT_SIZE/2 - 0.5f - x ) +
-				( DLIGHT_SIZE/2 - 0.5f - y ) * ( DLIGHT_SIZE/2 - 0.5f - y );
-			b = 4000 / d;
-			if (b > 255) {
-				b = 255;
-			} else if ( b < 75 ) {
-				b = 0;
+		// make a centered inverse-square falloff blob for dynamic lighting
+		for (x=0 ; x<DLIGHT_SIZE ; x++) {
+			for (y=0 ; y<DLIGHT_SIZE ; y++) {
+				float	d;
+
+				d = ( DLIGHT_SIZE/2 - 0.5f - x ) * ( DLIGHT_SIZE/2 - 0.5f - x ) +
+					( DLIGHT_SIZE/2 - 0.5f - y ) * ( DLIGHT_SIZE/2 - 0.5f - y );
+				b = 4000 / d;
+				if (b > 255) {
+					b = 255;
+				} else if ( b < 75 ) {
+					b = 0;
+				}
+				data[y][x][0] = 
+				data[y][x][1] = 
+				data[y][x][2] = b;
+				data[y][x][3] = 255;			
 			}
-			data[y][x][0] = 
-			data[y][x][1] = 
-			data[y][x][2] = b;
-			data[y][x][3] = 255;			
 		}
+		tr.dlightImage = R_CreateImage("*dlight", (byte *)data, DLIGHT_SIZE, DLIGHT_SIZE, qfalse, qfalse, GL_CLAMP );
 	}
-	tr.dlightImage = R_CreateImage("*dlight", (byte *)data, DLIGHT_SIZE, DLIGHT_SIZE, qfalse, qfalse, GL_CLAMP );
 }
 
 
